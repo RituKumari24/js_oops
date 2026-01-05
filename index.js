53b2e4d61470b268b1ef6868c84ca3ee2612a034
RituKumari24 <ritukumari22642@gmail.com>
2026-01-05 13:50:01 +0530

    Add JavaScript functionality and CSS styles for DOM manipulation examples

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-hed = document.querySelector("h2");
+body = document.querySelector("body");
 
+hed = document.querySelector("h2");
 hed.append(" - Welcome to JavaScript DOM Manipulation");
+h2 = document.createElement("h2");
+h2.innerHTML = "<h2> This is an append Example </h2>";
+// document.querySelector("body").append(h2);
+body.prepend(h2);
+
+
+
+btn = document.createElement("button");
+btn.innerText = "Click Me";
+
+btn.style.backgroundColor = "red";
+btn.style.color = "white";
+
+body.prepend(btn);
+
+
+// use of classList to add multiple classes to an element
+
+para = document.querySelector("p");
+
+para.classList.add("classTwo");
